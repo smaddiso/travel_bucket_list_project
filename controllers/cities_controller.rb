1519ce95bb210e2121a9613e44e2b69acbd40e67
Steve Maddison <stevemaddison@steves-mbp.home>
2020-03-20 12:59:09 +0000

    Updated cities_controller.rb and countries_controller.rb

---
+++ controllers/cities_controller.rb
@@ -0,0 +1,42 @@
+require("sinatra")
+require("sinatra/contrib/all")
+require("pry-byebug")
+
+require_relative("../models/city")
+require_relative("../models/country")
+also_reload("../models/*"")
+
+#CRUD - READ / index / GET
+get '/cities' do
+
+end
+
+#CRUD - CREATE  / new / GET
+get '/cities/new' do
+
+end
+
+#CRUD - READ / show / GET
+get '/cities/:id' do
+
+end
+
+#CRUD - CREATE/ create / POST
+post '/cities' do
+
+end
+
+#CRUD - UPDATE / edit / GET
+get '/citiess/:id/edit' do
+
+end
+
+#CRUD - UPDATE / update / POST
+post '/cities/:id' do
+
+end
+
+#CRUD - DELETE / delete / POST
+post '/cities/:id/delete' do
+
+end
---
+++ controllers/countries_controller.rb
@@ -0,0 +1,42 @@
+require("sinatra")
+require("sinatra/contrib/all")
+require("pry-byebug")
+
+require_relative("../models/city")
+require_relative("../models/country")
+also_reload("../models/*"")
+
+#CRUD - READ / index / GET
+get '/countries' do
+
+end
+
+#CRUD - CREATE  / new / GET
+get '/countries/new' do
+
+end
+
+#CRUD - READ / show / GET
+get '/countries/:id' do
+
+end
+
+#CRUD - CREATE/ create / POST
+post '/countries' do
+
+end
+
+#CRUD - UPDATE / edit / GET
+get '/countries/:id/edit' do
+
+end
+
+#CRUD - UPDATE / update / POST
+post '/countries/:id' do
+
+end
+
+#CRUD - DELETE / delete / POST
+post '/countries/:id/delete' do
+
+end
